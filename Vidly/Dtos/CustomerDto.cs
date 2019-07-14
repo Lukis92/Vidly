73a739c9e8f043fe181d03e9e35711b96b6a0e1b
Lukasz <lukas.korol@gmail.com>
2019-07-14 17:36:38 +0200

    Section 6 - WebAPI

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public bool IsSubscriedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        [Required]
+        public byte MembershipTypeID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public Genre Genre { get; set; }
+        [Required]
+        public int GenreID { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleasedDate { get; set; }
+
+        private DateTime addedDate = DateTime.Now;
+
+        public DateTime AddedDate
+        {
+            get
+            {
+                return addedDate;
+            }
+            set
+            {
+                addedDate = value;
+            }
+        }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
