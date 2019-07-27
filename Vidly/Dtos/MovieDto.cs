eaee8bfb89eba1d420966621d7aa6685a3769ff6
Lukasz <lukas.korol@gmail.com>
2019-07-27 20:24:35 +0200

    WebApi for movies.

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public int GenreID { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ReleasedDate { get; set; }
+
+        private DateTime addedDate = DateTime.Now;
+
+        public DateTime AddedDate
+        {
+            get
+            {
+                return addedDate;
+            }
+            set
+            {
+                addedDate = value;
+            }
+        }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
